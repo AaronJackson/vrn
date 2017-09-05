a068baea32afa13a81f798389d111c573871c240
Aaron S. Jackson <aaron.jackson@nottingham.ac.uk>
2017-09-05 17:06:30 +0100

    Added the download link for Adrian's landmark detector

--- download.sh
+++ download.sh
@@ -4,5 +4,5 @@ echo "This script will download the VRN network model."
 
 wget -O vrn-unguided.t7 \
      http://cs.nott.ac.uk/~psxasj/download.php?file=vrn-unguided.t7
-
-
+wget -O face-alignment/2D-FAN-300W.t7 \
+     https://www.adrianbulat.com/downloads/FaceAlignment/2D-FAN-300W.t7
