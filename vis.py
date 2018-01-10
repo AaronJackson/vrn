Fixed Z scaling (thanks cloudhan)
The training data was voxelised such that Z was twice as deep as X,Y are wide/tall. This was to try and make better use of the volume.

vis.py
@@ -12,10 +12,6 @@ parser.add_argument('--texture', dest='texture',
 
 args = parser.parse_args()
 
-# Worth mentioning that the Z component (axis=0) is twice as deep as
-# the X and Y components are wide/tall. I'm going to leave these
-# unscaled for now, but if you know how to scale the mesh from
-# showvol, I would love to know.
 vol = np.fromfile(args.volume, dtype=np.int8)
 vol = vol.reshape((200,192,192))
 
@@ -32,6 +28,7 @@ volRGB = np.stack(((vol > 1) * im[:,:,0],
                    (vol > 1) * im[:,:,2]), axis=3)
 
 v = vv.volshow(volRGB, renderStyle='iso')
+v.transformations[1].sz = 0.5 # Z was twice as deep during training
 
 l0 = vv.gca()
 l0.light0.ambient = 0.9 # 0.2 is default for light 0