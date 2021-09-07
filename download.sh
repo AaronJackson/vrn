ea5184770216e7c6f7ec2a1e1ccb0a17b5ecc4e6
Aaron Jackson <aaron@aaronsplace.co.uk>
2021-09-07 20:15:46 +0100

    Update model download links
    These are now hosted from my S3 bucket as the original UoN link had
    finally died. Both models have been cast to CPU floats which means a
    GPU is not necessary (although the casting actually took place for the
    docker version)

--- download.sh
+++ download.sh
@@ -3,6 +3,6 @@
 echo "This script will download the VRN network model."
 
 wget -O vrn-unguided.t7 \
-     http://cs.nott.ac.uk/~psxasj/download.php?file=vrn-unguided.t7
+     https://asjackson.s3.fr-par.scw.cloud/vrn/vrn-unguided.t7
 wget -O face-alignment/2D-FAN-300W.t7 \
-     https://www.adrianbulat.com/downloads/FaceAlignment/2D-FAN-300W.t7
+     https://asjackson.s3.fr-par.scw.cloud/vrn/2D-FAN-300W.t7
